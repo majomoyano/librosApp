f9e78a738c0c2df8480437c72248c10d387f6ca0
gjareval <gjareval@spol.edu.ec>
2023-08-29 15:53:20 -0500

    Cambios

--- src/app/providers/data-service.service.ts
+++ src/app/providers/data-service.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
-  private generoURL: string = 'http://localhost:3000/rest/generos/findAll/json';
-  private libroURL: string = 'http://localhost:3000/rest/libros/findAll/json';
+  private generoURL: string = 'https://libreria-production-3ebb.up.railway.app/rest/generos/findAll/json';
+  private libroURL: string = 'https://libreria-production-3ebb.up.railway.app/rest/libros/findAll/json';
 
   constructor(private http:HttpClient) { }
 
@@ -16,6 +16,11 @@ export class DataServiceService {
   }
 
   getLibroResponse() {
-    return this.http.get(this.libroURL);
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJhZG1pbiIsInJvbCI6ImFkbWluIiwiaWF0IjoxNjkzMzM4NzE4fQ.Tgej2i1pcHoEbfUQbOKZNTAFKzroztyeRlWp8SOC7ZA' // Reemplaza con tu token de acceso
+    });
+
+    return this.http.get(this.libroURL, { headers });
   }
 }
